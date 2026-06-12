f51f2b1f5b56399c98d34f6a0f303f289c5a9206
Mohamed Aflal <rafaideenmuhammadaflalmuhammed@gmail.com>
2026-06-13 00:48:00 +0530

    Add files via upload

---
+++ j14.js
@@ -0,0 +1,45 @@
+const display = document.getElementById("display");
+
+function appendValue(value)
+{
+    if(display.value === "0")
+    {
+        display.value = value;
+    }
+    else
+    {
+        display.value += value;
+    }
+}
+
+function clearDisplay()
+{
+    display.value = "0";
+}
+
+function backspace()
+{
+    display.value = display.value.slice(0, -1);
+
+    if(display.value === "")
+    {
+        display.value = "0";
+    }
+}
+
+function percentage()
+{
+    display.value = parseFloat(display.value) / 100;
+}
+
+function calculate()
+{
+    try
+    {
+        display.value = eval(display.value);
+    }
+    catch(error)
+    {
+        display.value = "Error";
+    }
+}
